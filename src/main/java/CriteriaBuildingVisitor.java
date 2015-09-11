3e1720dd5fafb0ea5f8c2dd99439090f6eaecce8
paul.rutledge <paul.rutledge@vodori.com>
2015-09-11 03:05:41 -0500

    bump

--- src/main/java/CriteriaBuildingVisitor.java
+++ src/main/java/CriteriaBuildingVisitor.java
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2015. Vodori, Inc.  All rights reserved.
- *
- * This software is not open source and license for its use and
- * modification can only be granted through an agreement with
- * Vodori, Inc.
- *
- * http://www.vodori.com
- */
-
 import cz.jirutka.rsql.parser.ast.AndNode;
 import cz.jirutka.rsql.parser.ast.ComparisonNode;
 import cz.jirutka.rsql.parser.ast.OrNode;
