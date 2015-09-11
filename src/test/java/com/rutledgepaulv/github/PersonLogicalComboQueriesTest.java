08e73dbaef76a79f34194a93ba5cd351c53c8bcb
paul.rutledge <paul.rutledge@vodori.com>
2015-09-11 07:07:50 -0500

    removing autogenerated

--- src/test/java/com/rutledgepaulv/github/PersonLogicalComboQueriesTest.java
+++ src/test/java/com/rutledgepaulv/github/PersonLogicalComboQueriesTest.java
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) 2015. Vodori, Inc.  All rights reserved.
- *
- * This software is not open source and license for its use and
- * modification can only be granted through an agreement with
- * Vodori, Inc.
- *
- * http://www.vodori.com
- */
 package com.rutledgepaulv.github;
 
 import com.rutledgepaulv.github.models.Person;
